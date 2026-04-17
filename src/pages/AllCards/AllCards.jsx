61277145b186edc44e60059cf54733761a26948c
Rakibul Islam <mdraislam56@gmail.com>
2026-04-17 12:10:53 +0600

    finish the project keen-keeper

--- src/pages/AllCards/AllCards.jsx
+++ src/pages/AllCards/AllCards.jsx
@@ -5,24 +5,16 @@ const allFriends = fetch("/friends.json").then((res) => res.json());
 
 const AllCards = () => {
     const friends = use(allFriends);
-    // console.log(friends);
-
 
     return (
-        <div className='container mx-auto '>
+        <div className='container mx-auto'>
             <h3 className='font-semibold text-xl pt-15'>Your Friends</h3>
 
             <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 mt-10'>
-                {
-                    friends.map(((friend,ind) => {
-                        return (
-                           <Cards key={ind} friend={friend}/>
-                        )
-                    }))
-                }
+                {friends.map((friend, ind) => (
+                    <Cards key={ind} friend={friend} />
+                ))}
             </div>
-
-
         </div>
     );
 };
