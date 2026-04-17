0662573d462dc04ac50218c15a8e0cd9403ddde2
Rakibul Islam <mdraislam56@gmail.com>
2026-04-17 15:52:12 +0600

    responsive footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,31 +3,33 @@ import { FaFacebook, FaInstagram, FaSquareXTwitter } from 'react-icons/fa6';
 
 const Footer = () => {
     return (
-        <footer className="footer footer-vertical grid sm:grid-cols-1 lg:grid-cols-2 bg-[#244D3F] text-primary-content mt-8 px-4 ">
-            <h1 className='text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-bold mt-6 sm:mt-8 md:mt-10'>KeenKeeper</h1>
+        <div className='mx-auto'>
+            <footer className="footer   bg-[#244D3F] p-10">
+                <div className='container mx-auto flex flex-col   gap-4 justify-center text-center items-center'>
+                    <h1 className='lg:text-5xl font-bold sm:text-sm text-white'>KeenKeeper</h1>
 
-            <p className="font-semibold text-gray-400 text-xs sm:text-sm md:text-base max-w-md">
-                Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.
-            </p>
-
-            <nav>
-                <h3 className='text-base sm:text-lg md:text-xl font-semibold'>Social Links</h3>
-                <div className="grid grid-flow-col gap-3 sm:gap-4 text-2xl sm:text-3xl md:text-4xl">
-                    <FaInstagram />
-                    <FaFacebook />
-                    <FaSquareXTwitter />
+                    <p className="font-semibold  text-white">
+                        Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.
+                    </p>
+                    <p className='text-white font-semibold text-3xl'>Social Link</p>
+                    <nav>
+                        <div className="grid grid-flow-col gap-4 text-white text-2xl">
+                            <FaInstagram />
+                            <FaFacebook />
+                            <FaSquareXTwitter />
+                        </div>
+                    </nav>
                 </div>
-            </nav>
-            <div className='flex flex-col sm:flex-row justify-between items-center gap-3 sm:gap-4 text-xs sm:text-sm text-gray-400 w-full mb-6 sm:mb-8 md:mb-10'>
-                <p> © {new Date().getFullYear()} - KeenKeeper. All rights reserved.</p>
-                <div className='flex flex-wrap justify-center sm:justify-end gap-2 sm:gap-3 md:gap-4 text-xs sm:text-sm text-gray-400'>
-                    <p>Privacy Policy</p>
-                    <p>Terms of Service</p>
-                    <p>Cookies</p>
+                <div className='container mx-auto sm:px-15 lg:flex sm:flex-row justify-between   text-white'>
+                    <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+                    <div className='flex sm:flex-row flex-col gap-4 justify-center items-center'>
+                        <p>Privacy Policy</p>
+                        <p>Terms of Service</p>
+                        <p>Cookies</p>
+                    </div>
                 </div>
-            </div>
-
-        </footer>
+            </footer>
+        </div>
     );
 };
 
